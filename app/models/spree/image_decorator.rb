d295d8109f3153b2b0b6e2049e77e0981dd0b854
Alex Costantini <therealcos@gmail.com>
2017-09-14 17:31:42 -0400

    wip

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -3,8 +3,8 @@ Spree::Image.class_eval do
 
   has_many :variant_images, class_name: '::Spree::VariantImage', dependent: :destroy
   has_many :variants, through: :variant_images
-  
-  validates :variants, :length => { :minimum => 1 }
+
+  validates :variants, :length => { :minimum => 1, :message => 'At least one variant is required' }
 
   def variant_html_classes
     variant_ids.map { |variant| "tmb-#{variant}"}.join(" ")
