0bc1d4f790cc3527d689d2cd4779735531c5480d
Alex Costantini <therealcos@gmail.com>
2017-09-14 16:34:38 -0400

    wip

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -2,7 +2,7 @@ Spree::Product.class_eval do
   has_many :nonuniq_variant_images, -> { order(:position) }, source: :images, through: :variants_including_master
 
   def variant_images
-    variant_images = Spree::VariantImage.where(variant_id: variant_ids)
+    variant_images = Spree::VariantImage.where(variant_id: variants_including_master_ids)
     Spree::Image.where(id: variant_images.pluck(:image_id), viewable_type: 'Spree::Variant').order(position: :asc)
   end
-end
+end	
