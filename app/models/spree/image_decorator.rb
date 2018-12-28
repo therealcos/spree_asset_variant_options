8f0ef68e7b1e3f8d62c335af8b8ff02f50284c83
Alex Costantini <therealcos@gmail.com>
2018-12-27 20:36:21 -1000

    wip

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -4,7 +4,13 @@ Spree::Image.class_eval do
   has_many :variant_images, class_name: '::Spree::VariantImage', dependent: :destroy
   has_many :variants, through: :variant_images
 
-  validates :variants, :length => { :minimum => 1, :message => "can't be blank" }
+  validates :variants_not_blank
+
+  def variants_not_blank
+  	if viewable_type == "Spree::Variant" && variants.length > 0
+  		errors.add(:variants, "Variants can't be blank")
+  	end
+  end
 
   def variant_html_classes
     variant_ids.map { |variant| "tmb-#{variant}"}.join(" ")
